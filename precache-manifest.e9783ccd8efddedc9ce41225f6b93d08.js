9c77b40ebc18417cdad022cd4177fbb3078caa03
andjhait <63277434+andjhait@users.noreply.github.com>
2020-04-07 10:40:51 +0530

    My commit

---
+++ precache-manifest.e9783ccd8efddedc9ce41225f6b93d08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "401556f3edbd817571fc964ae23c2a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65fd2f9365c22eafe5e6",
+    "url": "/static/css/main.b7e92f68.chunk.css"
+  },
+  {
+    "revision": "e1aaddcdd716034ec2d1",
+    "url": "/static/js/2.73c50fff.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/static/js/2.73c50fff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65fd2f9365c22eafe5e6",
+    "url": "/static/js/main.c94d13cf.chunk.js"
+  },
+  {
+    "revision": "f03d895f7550cba2a8da",
+    "url": "/static/js/runtime-main.914fab86.js"
+  },
+  {
+    "revision": "69cf4661b2d3a84726edaf762bb82f63",
+    "url": "/static/media/Gilroy-ExtraBold.69cf4661.ttf"
+  }
+]);
